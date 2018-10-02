9d7d95c48dfae6a2c3bb0a877b068b4ee861bb2e
admin <>
2018-10-02 00:17:59 -0700

    checkout form position changed

--- wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
@@ -29,18 +29,6 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', wc_get_checko
 	<div class="container">
 		<form name="checkout" method="post" class="checkout woocommerce-checkout <?php echo $wrapper_classes; ?>" action="<?php echo esc_url( $get_checkout_url ); ?>" enctype="multipart/form-data">
 			<div class="row">
-				<div class="col-xl-4 push-xl-8 col-lg-5 push-lg-7">
-					<div class="shadow bg-white stick-to-content mb-4">
-
-						<div class="bg-dark dark p-4"><h5 class="mb-0"><?php _e( 'Your order', 'woocommerce' ); ?></h5></div>
-						<div class="p-4">
-							<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-							<div id="order_review" class="woocommerce-checkout-review-order">
-								<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="col-xl-8 pull-xl-4 col-lg-7 pull-lg-5">
 					<div class="bg-white p-4 p-md-5 mb-4">
 						<?php if ( $checkout->get_checkout_fields() ) :?>
@@ -51,6 +39,18 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', wc_get_checko
 						</div>
 						<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 						<?php endif; ?>
+					</div>
+				</div>
+				<div class="col-xl-4 push-xl-8 col-lg-5 push-lg-7">
+					<div class="shadow bg-white stick-to-content mb-4">
+
+						<div class="bg-dark dark p-4"><h5 class="mb-0"><?php _e( 'Your order', 'woocommerce' ); ?></h5></div>
+						<div class="p-4">
+							<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+							<div id="order_review" class="woocommerce-checkout-review-order">
+								<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
